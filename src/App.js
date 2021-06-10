e86a9f02cbf84108fd6f89bae4f0a49932d406d3
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-09 23:01:24 -0300

    Create Header and main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 function App() {
   return (
-    <h1>Noticias</h1>
+    <Fragment>
+      <Header 
+        titulo='Buscador de noticias'
+      />
+
+      <div className="container white">
+        <h1>Formulario</h1>
+      </div>
+    </Fragment>
   );
 }
 
