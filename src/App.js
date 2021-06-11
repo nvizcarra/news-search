237508c57f89869f4ff38bd50c7cb4c51ee544e1
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-11 00:33:26 -0300

    Put category into state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 function App() {
+
+  // Definir la categoría y noticias
+  const [ categoria, guardarCategoria ] = useState('');
+
+
   return (
     <Fragment>
       <Header 
@@ -9,7 +14,9 @@ function App() {
       />
 
       <div className="container white">
-        <Formulario/>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
       </div>
     </Fragment>
   );
